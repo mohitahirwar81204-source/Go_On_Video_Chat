20f238699978e2fa48c27150a319018752c97ba5
mohitahirwar81204-source <mohitahirwar81204@gmail.com>
2026-07-26 14:41:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Store waiting users by gender preference
 // structure: { id, gender, targetGender }
